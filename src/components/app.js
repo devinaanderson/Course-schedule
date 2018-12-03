f875be38b6390a5f7641da1e34ade3574aef6ed5
devinaanderson <daa7896@gmail.com>
2018-12-03 15:58:34 -0700

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+ class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Course Scheduler</h1>
+        <h2>React Redux</h2>
+      </div>
+    );
+  }
+}
+
+
+export default App
