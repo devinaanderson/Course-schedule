f2fd364b4dc4eea7c335caf80e7cadee7b952334
devinaanderson <daa7896@gmail.com>
2018-12-10 09:04:12 -0700

    built course reducer and implemented mapStateToProps

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import courses from './couresReducers';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  
+  courses
 });
 
 export default rootReducer;
